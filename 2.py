c6497266a66cad92a2075f06d7fa3a3eeecdb04f
Vika <baghdasaryan.vika@gmail.com>
2020-01-19 21:41:41 +0400

    comment

---
+++ 2.py
@@ -0,0 +1,24 @@
+country_name = input("choose the country")
+print(country_name)
+city = input("choose the city")
+print(city)
+hotel =input("choose the hotel")
+print(hotel)
+stars = input("choose the stars")
+stars= int(stars)
+print (stars)
+persons =input("Enter the quantity")
+persons = int(persons)
+print(persons)
+days = input("enter the days")
+days = int(days)
+print (days)
+wish = ("Have a nice summer holidays")
+print(wish)
+
+
+
+
+
+
+
