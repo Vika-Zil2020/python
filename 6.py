ed9daf1deb0c9e121b9fffce89fd3f048b83df0d
Vika <baghdasaryan.vika@gmail.com>
2020-01-21 18:18:54 +0400

    comment

---
+++ 6.py
@@ -0,0 +1,2 @@
+my_string = "You never know how strong you are,until being strong is your only choice"
+print (" " and "str" in my_string or "oice" in my_string)
---
+++ 9.py
@@ -0,0 +1,3 @@
+year=input("Insert the year")
+year=int(year)
+print(year%4==0)
