57fa5325b85d44afe227c70182ea83d47b8e5fff
Vika <baghdasaryan.vika@gmail.com>
2020-02-14 15:02:50 +0400

    dic4

---
+++ dic4.py
@@ -0,0 +1,10 @@
+user_info = {}
+name = input("Input your name: ")
+surname = input("Input your surname: ")
+tel = input("Input your telephone: ")
+address = input("Input your address: ")
+user_info["name"] = name
+user_info["surname"] = surname
+user_info["tel"] = tel
+user_info["address"] =address
+print(user_info)
