f73988f072df34184a495e5243a17044889a724a
Vika <baghdasaryan.vika@gmail.com>
2020-02-14 15:01:53 +0400

    dic3

---
+++ dic3.py
@@ -0,0 +1,6 @@
+my_dic = {"name":"Vika","age":34,"city":"Yerevan"}
+i = input("input your key")
+if i in my_dic:
+	print("your key is in my dictionary")
+else:
+	print("your key isn't in my dictionary")
