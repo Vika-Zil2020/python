38484fa5f40b5673209af995dbaa8627b68dce65
Vika <baghdasaryan.vika@gmail.com>
2020-02-16 13:38:36 +0400

    ob3

---
+++ object3.py
@@ -0,0 +1,14 @@
+class Action:
+	def __init__(self):
+	    self.string = ""
+
+	def get_string(self):
+		self.string=input()
+
+	def print_string(self):
+		print(self.string.upper())
+
+string=Action()
+string.get_string()
+string.print_string()
+	
